bb62fb3bf46d3ca929cd20932aa0065b6aa2a8f3
Karim Essam <126638336+KaEssam@users.noreply.github.com>
2025-04-21 14:21:55 +0200

    [2145] [Time Beats: 90.91%] [Memory Beats: 36.36%] - LeetPush

---
+++ DCP-04-25/Count-the-Hidden-Sequences.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public long GridGame(int[][] grid) {
+        long minResult = long.MaxValue;
+        long row1Sum = 0;
+
+        foreach (var val in grid[0]) {
+            row1Sum += val;
+        }
+
+        long row2Sum = 0;
+
+        for (int i = 0; i < grid[0].Length; ++i) {
+            row1Sum -= grid[0][i];
+            minResult = Math.Min(minResult, Math.Max(row1Sum, row2Sum));
+            row2Sum += grid[1][i];
+        }
+
+        return minResult;
+    }
+}
