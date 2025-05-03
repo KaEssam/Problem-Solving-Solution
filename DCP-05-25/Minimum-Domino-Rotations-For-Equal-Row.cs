d6b41d695bf14da3c093ea97ce3f3cecc79b0cff
Karim Essam <126638336+KaEssam@users.noreply.github.com>
2025-05-03 19:45:33 +0300

    [1007] [Time Beats: 85.71%] [Memory Beats: 14.29%] - LeetPush

---
+++ DCP-05-25/Minimum-Domino-Rotations-For-Equal-Row.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int MinDominoRotations(int[] tops, int[] bottoms) {
+    int[] cnta = new int[7], cntb = new int[7], same = new int[7];
+    for (int i = 0; i < tops.Length; i++) {
+        cnta[tops[i]]++;
+        cntb[bottoms[i]]++;
+        if (tops[i] == bottoms[i]) same[tops[i]]++;
+    }
+    for (int i = 1; i <= 6; i++) {
+        if (cnta[i] + cntb[i] - same[i] == tops.Length) {
+            return Math.Min(cnta[i], cntb[i]) - same[i];
+        }
+    }
+    return -1;
+}
+}
