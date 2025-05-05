a89ebbccaaaf18b85ac67c1749aa384a82bfb66f
Karim Essam <126638336+KaEssam@users.noreply.github.com>
2025-05-05 15:54:33 +0300

    [790] [Time Beats: 100.00%] [Memory Beats: 97.98%] - LeetPush

---
+++ DCP-05-25/Domino-and-Tromino-Tiling.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int NumTilings(int n) {
+        int MOD = 1_000_000_007;
+        if (n == 0) return 1;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        long[] dp = new long[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (2 * dp[i - 1] + dp[i - 3]) % MOD;
+        }
+
+        return (int)dp[n];
+    }
+}
