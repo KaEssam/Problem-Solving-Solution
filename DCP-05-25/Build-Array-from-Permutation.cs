1e7c492201311732ef69e29133d023bfea535e62
Karim Essam <126638336+KaEssam@users.noreply.github.com>
2025-05-06 16:00:37 +0300

    [1920] [Time Beats: 100.00%] [Memory Beats: 8.76%] - LeetPush

---
+++ DCP-05-25/Build-Array-from-Permutation.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int[] BuildArray(int[] nums) {
+        int[] result = new int[nums.Length];
+        for (int i = 0; i < nums.Length; i++) {
+            result[i] = nums[nums[i]];
+        }
+        return result;
+    }
+}
